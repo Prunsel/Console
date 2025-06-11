214b47e9174d6d854f143e98e9354725ef0b17c9
Prunsel <thomas2010beans@gmail.com>
2025-06-11 19:40:14 +0100

    Extra functions + Pong Demo

--- cart/cart.lua
+++ cart/cart.lua
@@ -2,7 +2,7 @@
 -- Test cart
 
 function load()
-    time = 0
+    time = 1
     bx = 64
     by = 64
     bspd = 1
@@ -18,46 +18,58 @@ function load()
     scoresize2 = 1
     score2 = 0
 
-    background = model_load("cylinder.obj", "player.png")
+    background = model_load("cylinder.obj", "texture.png")
+    level = model_load("level.obj", "texture.png")
 
     particles = {}
+    font = font_load("fonts/CrimsonText-Bold.ttf")
+
 end
 
 function update(dt)
     time = time + 0.01
-
+    
     -- paddle 1
-    if btn_held(key_down) then p1y = p1y + pspeed end
-    if btn_held(key_up) then p1y = p1y - pspeed end
-    p1y = clamp(p1y, 2, 126 - pheight)
+    if key_held(key_down) or btn_held("down") then p1y = p1y + pspeed end
+    if key_held(key_up) or btn_held("up") then p1y = p1y - pspeed end
+    p1y = clamp(p1y, 2, 125 - pheight)
 
     -- paddle 2
     if by > p2y then p2y = p2y + pspeed
     elseif by < p2y + pheight then p2y = p2y - pspeed end
-    p2y = clamp(p2y, 2, 126 - pheight)
+    p2y = clamp(p2y, 2, 125 - pheight)
 
     -- ball update
     bx = bx + bxv * bspd
     by = by + byv * bspd
     if point_in_rectangle(bx,by, 8,p1y - 2, 1,pheight + 3) or point_in_rectangle(bx,by, 117,p2y - 2, 1,pheight + 3) then 
+        for i = 1, 10 do new_particle(bx, by, "bounce") end
         bxv = -bxv
         byv = (byv / abs(byv)) * random(0.8, 1.5)
     end
-    if by <= 3 then byv = -byv end
-    if by >= 124 then byv = -byv end
-    if bx >= 124 then 
-        bx = 64
-        by = 64
-        byv = 0.5
-        bxv = -1
-        score1 = score1 + 1
-        scoresize1 = 3  
+    if by <= 3 then 
+        byv = -byv 
+        for i = 1, 10 do new_particle(bx,by, "bounce2") end
     end
-    if bx <= 0 then 
+    if by >= 124 then 
+        byv = -byv
+        for i = 1, 10 do new_particle(bx,by, "bounce2") end 
+    end
+    if bx >= 124 then
+        for i = 1, 10 do new_particle(bx,by,"score") end
         bx = 64
         by = 64
-        byv = 0.5
+        byv = ((p2y - by) / abs(p2y - by)) / 2
         bxv = 1
+        score1 = score1 + 1
+        scoresize1 = 3
+    end
+    if bx <= 0 then
+        for i = 1, 10 do new_particle(bx,by,"score") end
+        bx = 64
+        by = 64
+        byv = ((p1y - by) / abs(p1y - by)) / 2
+        bxv = -1
         score2 = score2 + 1
         scoresize2 = 3
     end
@@ -66,8 +78,12 @@ function update(dt)
     new_particle(bx,by,"trail")
 
     -- update score text
-    if scoresize1 > 1 then scoresize1 = scoresize1 - 0.1 end
-    if scoresize2 > 1 then scoresize2 = scoresize2 - 0.1 end
+    if scoresize1 > 1 then
+        scoresize1 = scoresize1 - 0.1 
+    end
+    if scoresize2 > 1 then
+        scoresize2 = scoresize2 - 0.1 
+    end
 
     -- update particles
     update_particles()
@@ -75,21 +91,24 @@ end
 
 function draw()
     model_draw(background, 20, 0, -30 + sin(time * 2) * 5, pi / 2, 0, time, 30, 30, 30)
-    rectangle_fill(0,0, 128,128, {0,0,0, 0.9})
+    rectangle_fill(0,0, 128,128, {0,0,0, 0.8})
 
     -- draw paddles
-    rectangle_fill(4,p1y, pwidth,pheight, colors.white)
-    rectangle_fill(121,p2y, pwidth,pheight, colors.white)
+    rectangle_fill(4,p1y + 1, pwidth,pheight - 1, colors.white)
+    rectangle_fill(5,p1y, pwidth / 2, pheight + 1, colors.light_grey)
+    rectangle_fill(120,p2y + 1, pwidth,pheight - 1, colors.white)
+    rectangle_fill(121,p2y, pwidth / 2, pheight + 1, colors.light_grey)
     
     -- draw line
-    line(64,0, 64,128)
+    line(64,0, 64,128, colors.light_grey)
 
     -- draw scores
+    color_set(colors.white)
     if scoresize1 > 1 then color_set(colors.yellow) end
-    print(score1, 32, 20, 0, scoresize1)
+    print(score1, 32, 20, 0, scoresize1, scoresize1, font)
     color_set(colors.white)
     if scoresize2 > 1 then color_set(colors.yellow) end
-    print(score2, 90, 20, 0, scoresize2)
+    print(score2, 90, 20, 0, scoresize2, scoresize2, font)
     color_set(colors.white)
     
     -- draw ball
@@ -101,6 +120,8 @@ function draw()
     -- particle draw
     draw_particles()
 
+    -- reset colour
+    color_set(colors.white)
 end
 
 function new_particle(x, y, type)
@@ -109,6 +130,13 @@ function new_particle(x, y, type)
     new.y = y
     new.type = type
     new.age = 0
+    if new.type == "score" or new.type == "bounce" then
+        new.xv = random(-bxv * 10,(-bxv + 1) * 10) / 10
+        new.yv = random(-10, 10) / 10
+    elseif new.type == "bounce2" then
+        new.xv = random(-10, 10) / 10
+        new.yv = random(byv * 10,(byv + 1) * 10) / 20
+    end
     array_add(particles, new)
 end
 
@@ -117,8 +145,12 @@ function draw_particles()
         local this = particles[i]
         if this.type == "trail" then
             circle_fill(this.x, this.y, 4 - this.age / 8, {1,0,0, (31 - this.age) / 31})
-        else
-
+        elseif this.type == "score" then
+            circle_fill(this.x, this.y, 5 - this.age / 5 + 1, colors.white)
+            circle_fill(this.x, this.y, 5 - this.age / 5, colors.red)
+        elseif this.type == "bounce" or "bounce2" then
+            circle_fill(this.x, this.y, 3 - this.age / 8 + 1, colors.grey)
+            circle_fill(this.x, this.y, 3 - this.age / 8, colors.white)
         end
 
     end
@@ -130,8 +162,15 @@ function update_particles()
         this.age = this.age + 1
         if this.type == "trail" then
             if this.age > 31 then array_delete(particles, i) end
-        else
-
+        elseif this.type == "score" or "bounce" then
+            if this.age > 24 then array_delete(particles, i) end
+            this.x = this.x + this.xv
+            this.y = this.y + this.yv
+            --this.yv = this.yv + 0.1
+        elseif this.type == "bounce2" then
+            if this.age > 24 then array_delete(particles, i) end
+            this.x = this.x + this.xv
+            this.y = this.y + this.yv
         end
 
     end
--- main.lua
+++ main.lua
@@ -7,9 +7,6 @@ g3d = require("lib/g3d")
 -- remove blur
 love.graphics.setDefaultFilter("nearest", "nearest")
 
--- pixel lines
-love.graphics.setLineStyle("rough")
-
 -- resolution
 res = 128
 
@@ -18,6 +15,7 @@ love.window.setFullscreen(true)
 scale = love.graphics.getHeight() / res
 transform_x = love.graphics.getWidth() / scale / 2 - (res / 2)
 love.graphics.setPointSize(scale)
+love.graphics.setLineStyle("rough")
 
 -- button mapping
 button_map = {
@@ -30,8 +28,35 @@ button_map = {
 }
 pressed_buttons = {}
 
--- built in functions for the 
+-- gamepad buttons
+gamepad_buttons = {
+    x = false,
+    z = false,
+    up = false,
+    down = false,
+    left = false,
+    right = false,
+}
+
+-- is cart playing
+cart_playing = false
+
+-- built in functions for the console
 enviroment = {
+
+    -- \/ ##### Resolution ##### \/
+
+    set_resolution = function(n)
+        res = n
+        scale = love.graphics.getHeight() / res
+        transform_x = love.graphics.getWidth() / scale / 2 - (res / 2)
+        love.graphics.setPointSize(scale)
+        canvas = love.graphics.newCanvas(res, res)
+    end,
+
+    get_resolution = function(n)
+        return res
+    end,
 
     -- \/ ##### Buttons ##### \/
 
@@ -42,16 +67,26 @@ enviroment = {
     key_Z = "z",
     key_X = "x",
 
-    btn_held = function(button)
+    key_held = function(button)
         return love.keyboard.isDown(button)
     end,
 
-    btn_press = function(button)
+    key_press = function(button)
         if pressed_buttons[button] then
             pressed_buttons[button] = false
             return true
         else
             return false
+        end
+    end,
+
+    btn_held = function(button)
+        if love.joystick.getJoysticks()[1] then
+            if button == "up" then return (love.joystick.getJoysticks()[1]:getAxis(2) == -1) end
+            if button == "down" then return (love.joystick.getJoysticks()[1]:getAxis(2) == 1) end
+            if button == "left" then return (love.joystick.getJoysticks()[1]:getAxis(1) == -1) end
+            if button == "right" then return (love.joystick.getJoysticks()[1]:getAxis(1) == 1) end
+            return love.joystick.getJoysticks()[1]:isDown(button)
         end
     end,
 
@@ -157,17 +192,34 @@ enviroment = {
         table.remove(array, n)
     end,
 
+    array_random = function(array)
+        return array[floor(love.math.random(1, #array))]
+    end,
+
+    -- \/ ##### Text ##### \/
+
+    print = function(text, x, y, r, sx, sy, font, col)
+        if font then love.graphics.setFont(font) end
+        if col then love.graphics.setColor(col) end
+        love.graphics.print(text, x, y, r, sx, sy)
+    end,
+
+    font_load = function(filepath)
+        local font = love.graphics.newFont("cart/"..filepath)
+        font:setFilter("nearest", "nearest")
+        return font
+    end,
+
     -- \/ ##### 2D Collision ##### \/
 
     point_in_rectangle = function(x, y, x2, y2, w, h)
         return x >= x2 and x <= x2 + w and y >= y2 and y <= y2 + h
     end,
 
-    -- \/ ##### Text ##### \/
+    -- \/ ##### 3D Collision ##### \/
 
-    print = function(text, x, y, r, sx, sy, col)
-        if col then love.graphics.setColor(col) end
-        love.graphics.print(text, x, y, r, sx, sy)
+    point_in_cuboid = function(x, y, z, x2, y2, z2, w, h, d)
+        return x >= x2 and x <= x2 + w and y >= y2 and y <= y2 + h and z >= z2 and z <= z2 + d
     end,
 
     -- \/ ##### 2D Shapes ##### \/
@@ -236,33 +288,30 @@ love.graphics.setColor(1,1,1, 1)
 
 -- set bad cart to false
 bad_cart = false
+
+-- menu things
 bad_cart_image = love.graphics.newImage("assets/sprites/bad_cart.png")
+no_cart_image = love.graphics.newImage("assets/sprites/no_cart.png")
 
 -- load
 function love.load()
-
-    -- load cart
-    if loadfile("cart/cart.lua") then
-        cart = loadfile("cart/cart.lua")
-        setfenv(cart, enviroment)
-        cart()
-        cart_load = enviroment.load
-        cart_update = enviroment.update
-        cart_draw = enviroment.draw
-        cart_load()
-    else
-        bad_cart = true
-    end
-
     -- canvas
     canvas = love.graphics.newCanvas(res, res)
+
+    -- load cart
+    load_cart()
 
 end
 
 
 -- update
 function love.update(dt)
-    if cart_update then cart_update(dt) end
+    -- cart update
+    if cart_update and cart_playing then cart_update(dt)
+    else
+        if love.keyboard.isDown("z") or love.joystick.getJoysticks()[1] and love.joystick.getJoysticks()[1]:isDown(2) then load_cart() end
+    end
+    
 end
 
 
@@ -272,10 +321,15 @@ function love.draw()
     -- set canvas
     love.graphics.setCanvas(canvas)
     love.graphics.clear()
-    
+
     -- draw
-    if cart_draw then cart_draw() end
-    if bad_cart == true then love.graphics.draw(bad_cart_image, 0, 0) end
+    if cart_draw and cart_playing then cart_draw()
+    elseif bad_cart == false then
+        love.graphics.rectangle("line", 1,1, 127,127)
+        love.graphics.draw(no_cart_image, 0, 0, 0, res / no_cart_image:getWidth())
+    elseif bad_cart == true then
+        love.graphics.draw(bad_cart_image, 32, 0, 0, 2)
+    end
 
     -- reset colour
     love.graphics.setColor(1,1,1,1)
@@ -290,6 +344,24 @@ function love.draw()
     -- draw canvas
     love.graphics.draw(canvas, 0, 0)
 
+end
+
+-- load cart
+function load_cart()
+    -- load cart
+    if loadfile("cart/cart.lua") then
+        cart = loadfile("cart/cart.lua")
+        setfenv(cart, enviroment)
+        cart()
+        cart_load = enviroment.load
+        cart_update = enviroment.update
+        cart_draw = enviroment.draw
+        cart_load()
+        cart_playing = true
+        bad_cart = false
+    elseif not love.filesystem.getRealDirectory("cart/cart.lua") then
+        bad_cart = false
+    end
 end
 
 -- keypressed
